162ed2ff9c1e93d260111d4ef93bdd8c30e2adbc
Louis Lamont <lamontlouis4@gmail.com>
2022-06-09 14:41:32 +1000

    test

--- set2/exercise0.py
+++ set2/exercise0.py
@@ -42,8 +42,12 @@ def add_5(a_number):
         return a_number plus five
     except expressed in python, not english
     """
-    the_answer = None
+    
+    the_answer = a_number + 5
     return the_answer
+
+    
+    
 
 
 def adder(a_number, another_number):
@@ -51,7 +55,7 @@ def adder(a_number, another_number):
 
     Same as above, but with any two numbers.
     """
-    the_answer = None
+    the_answer = a_number + another_number
     return the_answer
 
 
