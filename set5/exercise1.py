0a96e1622260f084684fcdd4338536185c850ccb
Louis Lamont <lamontlouis4@gmail.com>
2022-07-14 18:57:42 +1000

    set5 exs

--- set5/exercise1.py
+++ set5/exercise1.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 
+from email import message
 import requests
 
 """REFACTORING
@@ -25,37 +26,39 @@ Modify this function, don't write a whole new one.
 
 
 def wordy_pyramid():
-    baseURL = (
-        "https://us-central1-waldenpondpress.cloudfunctions.net/"
-        "give_me_a_word?wordlength={length}"
-    )
+    
     pyramid_list = []
-    for i in range(3, 21, 2):
-        url = baseURL.format(length=i)
-        r = requests.get(url)
-        if r.status_code is 200:
-            message = r.text
-            pyramid_list.append(message)
-        else:
-            print("failed a request", r.status_code, i)
-    for i in range(20, 3, -2):
-        url = baseURL.format(length=i)
-        r = requests.get(url)
-        if r.status_code is 200:
-            message = r.text
-            pyramid_list.append(message)
-        else:
-            print("failed a request", r.status_code, i)
+    
+    first_list = list_of_words_with_lengths(list_of_lengths=list(range(3, 21, 2)))
+    second_list = list_of_words_with_lengths(list_of_lengths=list(range(20, 3, -2)))
 
+    pyramid_list = first_list + second_list
+    
     return pyramid_list
 
 
 def get_a_word_of_length_n(length):
-    pass
+
+    baseURL = (
+        "https://us-central1-waldenpondpress.cloudfunctions.net/"
+        "give_me_a_word?wordlength={length}"
+    )
+
+    url = baseURL.format(length=length)
+    r = requests.get(url)
+    if r.status_code is 200:
+        message = r.text
+        return message
 
 
 def list_of_words_with_lengths(list_of_lengths):
-    pass
+    pyramid_list = []
+
+    for i in list_of_lengths:
+        message = get_a_word_of_length_n(length = i)
+        pyramid_list.append(message)
+
+    return pyramid_list
 
 
 if __name__ == "__main__":
--- set5/readme.md
+++ set5/readme.md
@@ -1,1 +1,4 @@
 TODO: Reflect on what you learned this week and what is still unclear.
+
+
+how good is the range() function woo hoo
